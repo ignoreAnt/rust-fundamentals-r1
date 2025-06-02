cc863ce66bffdee32395ee9904d912b2c45ff92d
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-02 20:08:56 +0200

    Update large_number_readability_g.rs

--- examples/large_number_readability_g.rs
+++ examples/large_number_readability_g.rs
@@ -1,1 +1,13 @@
+fn main() { 
+  let num1 = 250;
+  let num2 = 75;
+
+  let sum = num1 + num2;
+  let difference = num1 -num2;
+  let product = num1 * num2;
+  let quotient = num1 / num2; //
+Integer division
+
+  println!("
+
 
