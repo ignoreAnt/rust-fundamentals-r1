e3bf0475c88e61a36d585410cc25f401a55a23af
Aakash B <aakash.bamne@gmail.com>
2025-06-03 19:35:28 +0530

    changes the file name for the practice problems

--- examples/large_number_readability_g.rs
+++ examples/large_number_readability_g.rs
@@ -1,17 +1,4 @@
-fn main() { 
-  let num1 = 250;
-  let num2 = 75;
-
-  let sum = num1 + num2;
-  let difference = num1 -num2;
-  let product = num1 * num2;
-  let quotient = num1 / num2; //
-Integer division
-
-  println!("Sum; {}", sum);
-  println!("Difference; {}", difference);
-  println!("Product: {}", product);
-  println! ("Quotient (integer division): {}", quotient);
+fn main() {
+    let city_population = 12_345_678;
+    println!("City Population: {}", city_population);
 }
-
-
