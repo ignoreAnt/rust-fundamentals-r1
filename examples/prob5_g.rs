25fe500a069045118a1073189769995c89f4423d
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-06 13:55:02 +0200

    prob5_g.rs

--- examples/prob5_g.rs
+++ examples/prob5_g.rs
@@ -1,1 +1,12 @@
+fn main() {
 
+    let my_char: char = '7';
+
+    println!("Is '{}' alphabetic? {}", my_char, my_char.is_alphabetic());
+
+    println!("Is '{}' numeric? {}", my_char, my_char.is_numeric());
+
+    let letter_char: char = 'P';
+
+    println!("Is '{}' uppercase? {}", letter_char, letter_char.is_uppercase());
+}
