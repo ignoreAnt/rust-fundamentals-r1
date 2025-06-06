f6a5526f5df4c816adb4d6fb1f8a2f283d1e4455
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-06 13:53:43 +0200

    prob4_g.rs

--- examples/prob4_g.rs
+++ examples/prob4_g.rs
@@ -1,1 +1,11 @@
+fn main() {
 
+    let is_sunny = true;
+    let is_weekend = false;
+
+    println!("Sunny weekend: {}", is_sunny && is_weekend); // false
+
+    println!("Sunny or weekend: {}", is_sunny || is_weekend); // true
+
+    println!("Not sunny: {}", !is_sunny); // false
+}
