8696694d9296b5838615244d3a96e7c23bf35b1a
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-07 12:27:35 +0200

    prob8_g.rs

---
+++ examples/prob8_g.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 10;
+    let y = 15;
+
+    println!("x == y: {}", x == y);
+    println!("x != y: {}", x != y);
+    println!("x < y: {}", x < y);
+    println!("x > y: {}", x > y);
+    println!("x <= 10: {}", x <= 10);
+    println!("y >= 15: {}", y >= 15);
+}
