22a105640921d0ed23dad45322cd6ab4406d9c66
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-07 12:29:19 +0200

    prob10_g.rs

--- examples/prob10_g.rs
+++ examples/prob10_g.rs
@@ -1,1 +1,8 @@
+fn main() {
+    let months = ["January", "February", "March"];
+
+    println!("First month: {}", months[0]);
+    println!("Third month: {}", months[2]);
+    println!("Length of the array: {}", months.len());
+}
 
