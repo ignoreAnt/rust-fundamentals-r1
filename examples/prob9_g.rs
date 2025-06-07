537c430113b3b30e42e52213a6a65fbc2180109b
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-07 12:28:07 +0200

    prob9_g.rs

--- examples/prob9_g.rs
+++ examples/prob9_g.rs
@@ -1,1 +1,7 @@
+fn main() {
+    let temperature_celsius: f64 = 25.5;
 
+    let is_comfortable = temperature_celsius >= 20.0 && temperature_celsius <= 26.0;
+
+    println!("Is the temperature comfortable? {}", is_comfortable);
+}
