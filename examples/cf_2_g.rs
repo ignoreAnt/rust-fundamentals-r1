0a3f2bffe82d8a3bb276fa83ebc269dc3b01cb41
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-21 12:07:46 +0200

    cf_2_g.rs

---
+++ examples/cf_2_g.rs
@@ -0,0 +1,8 @@
+fn product_of_three(a: f64, b: f64, c: f64) -> f64 {
+    a * b * c
+}
+
+fn main() {
+    let result = product_of_three(2.0, 3.5, 4.0);
+    println!("The product is: {}", result);
+}
