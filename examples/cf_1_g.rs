a74b165d077de7f6ea9c445ea07aa83da6c36894
Akshitha5678 <akshithagottumukkala2006@gmail.com>
2025-06-21 12:07:26 +0200

    cf_1_g.rs

---
+++ examples/cf_1_g.rs
@@ -0,0 +1,9 @@
+fn print_startup_message() {
+    println!("Welcome to StellarApp!");
+    println!("Initializing system components...");
+    println!("Ready to launch 🚀");
+}
+
+fn main() {
+    print_startup_message();
+}
